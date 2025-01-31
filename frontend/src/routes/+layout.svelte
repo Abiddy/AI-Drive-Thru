a6c303e1570cc76df3c894d7887ca04e57358eb4
Abiddy <abidinouman@gmail.com>
2025-01-31 00:29:44 -0800

    fixing build

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,10 +1,9 @@
 <script lang="ts">
 	import '../app.css';
-	let { children } = $props();
 </script>
 
 <div class="min-h-screen bg-background">
 	<main class="container mx-auto p-4">
-		{@render children()}
+		<slot />
 	</main>
 </div>
